4104f6c8a81d1f56bbef1f8060a1d4531d63db31
arunSurya-2812 <81164749+arunSurya-2812@users.noreply.github.com>
2022-03-30 15:06:05 +0530

    set footer and route link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,10 +42,10 @@ export default function Navbar() {
               <a href="#blog">Blog</a>
             </li>
             <li>
-            <a href="https://admin.efleet.world/">Contact</a>
+              <a href="#footer">Contact</a>
             </li>
             <li>
-            <a href="https://admin.efleet.world/Register">Register</a>
+              <a href="https://admin.efleet.world/">Register</a>
             </li>
           </ul>
         </div>
